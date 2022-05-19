d08c1a39b97c7798a3203967a14f81a7d51d4d79
Leo Liu <karta1041337@gmail.com>
2022-05-20 00:25:05 +0800

    Fix unit test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,6 @@
-import {
-    App as CdkApp,
-} from 'aws-cdk-lib';
-
-import * as src from '../src/';
-
-describe('Access Point', () => {
-    // GIVEN
-    const app = new CdkApp();
-
-    // WHEN
-
-    // THEN
-    describe('should pass', () => {
-        it('mock', () => {
-            expect(true).toBeTruthy();
-        });
+// eslint-disable-next-line import/unambiguous
+describe('should pass', () => {
+    it('mock', () => {
+        expect(true).toBeTruthy();
     });
 });
